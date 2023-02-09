f66e7f21f2c131330fb4170fd818a57b63a2c48b
dmitryShirokov2098 <rikudo10@mail.ru>
2023-02-10 00:30:31 +0300

    completed step 5

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dmitryShirokov2098/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/dmitryShirokov2098/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/ee52da672b8d72d292a5/maintainability)](https://codeclimate.com/github/dmitryShirokov2098/frontend-project-44/maintainability)
+[![asciicast](https://asciinema.org/a/fMEJTb9A9P0uYrnr0H8tBHrkJ.svg)](https://asciinema.org/a/fMEJTb9A9P0uYrnr0H8tBHrkJ)
--- src/Games/even.js
+++ src/Games/even.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 import greeting from '../cli.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-console.log(rules);
 
 const getRandom = () => {
   const min = Math.ceil(1);
@@ -15,6 +14,7 @@ const isEven = (numb) => ((numb % 2 === 0) ? 'yes' : 'no');
 
 const isEvenGame = () => {
   const userName = greeting();
+  console.log(rules);
   for (let i = 0; i < 3; i += 1) {
     const inputNumber = getRandom();
     console.log(`Question: ${inputNumber}`);
@@ -25,9 +25,9 @@ const isEvenGame = () => {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${check}'.`);
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
   }
-  return `Congratulations, ${userName}!`;
+  console.log(`Congratulations, ${userName}!`);
 };
-isEvenGame();
+export default isEvenGame;
